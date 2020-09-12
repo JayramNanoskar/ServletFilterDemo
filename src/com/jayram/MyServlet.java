2e8272637888ece71630f586b925ead0363188c3
JayramNanoskar <jayram.n978@gmail.com>
2020-09-12 18:41:09 +0530

    initial commit

---
+++ src/com/jayram/MyServlet.java
@@ -0,0 +1,21 @@
+package com.jayram;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns="/doLogin")
+public class MyServlet extends HttpServlet{
+protected void doGet(HttpServletRequest req , HttpServletResponse res) throws ServletException, IOException {
+		
+	PrintWriter out = res.getWriter();
+	String username = req.getParameter("uname");
+	String password  = req.getParameter("pwd");
+	out.println(username);
+}
+}
